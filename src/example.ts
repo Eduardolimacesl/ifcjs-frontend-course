e1be3e0911d03c8c103a22071f11c766de57cea7
@eduardoLima <eduardoLimacesl@gmail.com>
2023-01-30 17:07:38 -0300

    chore: firsts contents

---
+++ src/example.ts
@@ -0,0 +1,9 @@
+const helloWorld:string = '3'
+const foo:Array<number> = [1,2,3,4]
+
+type Car = {
+  doors: number;
+  plate: string;
+  airConditionerModel?: string;
+}
+const car: Car = { doors: 4, plate: "myPlate" }
